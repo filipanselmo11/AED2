8ebf433b0f6b22c22fae99848d321ee1519020d7
filipanselmo11 <filip_anselmo@yahoo.com.br>
2017-10-12 15:53:17 -0400

    algoritmo de casamento de strings

---
+++ stringmatch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void stringmatchIngenuo(string text, string padrao){
+    int n = text.size();
+    int m = padrao.size();
+    for(int s = 0; s < n - m; s++){
+        for(int i = 0; i < n; i++){
+            if(){
+                
+            }
+        }
+    }
+}
