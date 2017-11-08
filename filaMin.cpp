c8e00bbdc02057571cb0febc4d2adbe3dc60243d
filip <filip@localhost.localdomain>
2017-11-08 17:18:53 -0400

    Fila de prioridade mínima (atualizado)

--- filaMin.cpp
+++ filaMin.cpp
@@ -13,16 +13,19 @@ private:
     int Pai(int i);
     int Esq(int i);
     int Dir(int i);
+    void insereChave(int i, int chave);
 public:
     Heap(){
         v.push_back(NIL);
     }
     void heapFica(int i);
     void constroiHeap();
-    void extractMin();
+    int extractMin();
     void insere(int elemento);
+    void insereMin(int chave);
     void print();
     void executa();
+    int heapMin();
 };
 
 int Heap::Pai(int i){
@@ -35,6 +38,10 @@ int Heap::Esq(int i){
 
 int Heap::Dir(int i){
     return 2 * i + 1;
+}
+
+int Heap::heapMin(){
+    return v[1];
 }
 
 void Heap::heapFica(int i){
@@ -61,10 +68,43 @@ void Heap::constroiHeap(){
 	}
 }
 
+int Heap::extractMin(){
+    int min;
+    if(tamheap < 1){
+        //cout << "Heap underflow" << endl;
+        cout << "Fluxo subterrâneo" << endl;
+    }
+    min = v[1];
+    v[1] = v[tamheap];
+    tamheap = v.size() + 1;
+    v.erase(v.begin());
+    heapFica(1);
+    //v.erase(v.begin());
+    return min;
+}
+
 void Heap::insere(int elemento){
         v.push_back(elemento);
-        tamheap = v.size();
         heapFica(elemento);
+        tamheap += 1;
+}
+
+void Heap::insereChave(int i, int chave){
+    if(chave < v[i]){
+        //cout << "erro : new key is smaller than current key" << endl;
+        cout << "erro : nova chave é menor do que a chave atual" << endl;
+    }
+    v[i] = chave;
+    while(i > 1 && v[Pai(i)] < v[i]){
+        swap(v[i], v[Pai(i)]);
+        i = Pai(i);
+    }
+}
+
+void Heap::insereMin(int chave){
+    tamheap = v.size() + 1;
+    v[tamheap] = -INT_MAX;
+    insereChave(tamheap,chave);
 }
 
 void Heap::print(){
@@ -77,19 +117,31 @@ void Heap::print(){
 void Heap::executa(){
     constroiHeap();
     print();
+    //cout << "Heap-Mínimo: " << heapMin() << endl;
 }
 
 int main(){
     Heap heap;
-    //heap.print();
-   // heap.insere(v,3);
-    //heap.print(v);
-    //heap.constroiHeap();
-    //heap.print();
-    //heap.insere(1);
-    //heap.constroiHeap();
-    //heap.print();
+    heap.insere(15);
+    heap.insere(13);
+    heap.insere(9);
+    heap.insere(5);
+    heap.insere(12);
+    heap.insere(8);
+    heap.insere(7);
+    heap.insere(4);
+    heap.insere(6);
+    heap.insere(2);
     heap.insere(1);
+    heap.executa();
+    cout << "---------------------------------------------------------------------------" << endl;
+    cout << "Heap-Mínimo-Extraído: " << heap.extractMin() << endl;
+    heap.executa();
+    cout << "Heap-Mínimo-Extraído: " << heap.extractMin() << endl;
+    heap.executa();
+    cout << "Heap-Mínimo-Extraído: " << heap.extractMin() << endl;
+    heap.executa();
+    cout << "Heap-Mínimo-Extraído: " << heap.extractMin() << endl;
     heap.executa();
     return 0;
 }
